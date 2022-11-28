152c2dadc866046361927fe9a861be1e6a04f2b8
ThibaudTGD <thibaud.ollivier@thegooddrive.net>
2022-11-28 01:06:10 +0100

    setup for aoc 2022

---
+++ Days/Day_2022_01.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day_2022_01 : DayScript2022
+{
+    protected override string part_1()
+    {
+        return base.part_1();
+    }
+
+    protected override string part_2()
+    {
+        return base.part_2();
+    }
+}
