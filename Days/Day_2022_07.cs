a152a8d9ca6285779c3c06b698ece20147713342
ThibaudTGD <thibaud.ollivier@thegooddrive.net>
2022-12-07 11:57:23 +0100

    Day 7

--- Days/Day_2022_07.cs
+++ Days/Day_2022_07.cs
@@ -6,9 +6,53 @@ using System.Linq;
 
 public class Day_2022_07 : DayScript2022
 {
+    #region Classes 
+        class File
+        {
+            public string name;
+            public double size;
+            public File(string n, double s) { name = n; size = s; }
+            public string ToString(NumberFormatInfo nfi) { return "File '" + name + "' of size " + size.ToString("N", nfi); }
+        }
+
+        class Dir
+        {
+            public Dir parent;
+            public string name;
+            public List<File> files;
+            public List<Dir> dirs;
+            double size = -1;
+
+            public Dir(Dir dad, string n)
+            {
+                parent = dad;
+                name = n;
+                files = new List<File>();
+                dirs = new List<Dir>();
+                size = -1;
+            }
+
+            public double getSize(int offset = 0)
+            {
+                // if size >= 0, that means size has already be computed so no need to do that again as we have no filesystem change after init
+                if (size < 0)
+                {
+                    double res = 0;
+                    files.ForEach(x => res += x.size);
+                    dirs.ForEach(x => res += x.getSize(offset + 1));
+
+                    size = res;
+                    //Debug.Log(Tools2022.WriteOffset(offset) + "Dir " + name + " has size " + size);
+                }
+
+                return size;
+            }
+        }
+    #endregion
+
     protected override string part_1()
     {
-        Dir mainDir = initFilesystem();
+        Dir mainDir = initFilesystem(); // read input and init FS
         mainDir.getSize();  //calc size of all dirs/subdirs
 
         return getSumOfSmallDirs(mainDir, 100000).ToString();
@@ -16,7 +60,7 @@ public class Day_2022_07 : DayScript2022
 
     protected override string part_2()
     {
-        Dir mainDir = initFilesystem();
+        Dir mainDir = initFilesystem(); // read input and init FS
         mainDir.getSize();  //calc size of all dirs/subdirs
 
         Debug.Log("Main dir size is " + mainDir.getSize() + " so, we look for smallest > " + (30000000 - (70000000 - mainDir.getSize())).ToString());
@@ -27,28 +71,6 @@ public class Day_2022_07 : DayScript2022
         // Debug.Log(" ------------------------------------------- ");
 
         return getSmallestDirSizeBiggerThan(30000000 - (70000000 - mainDir.getSize()), mainDir).ToString();
-    }
-
-    /// <summary>
-    /// Debug function to draw filesystem tree
-    /// </summary>
-    /// <param name="currentDir">current dir to display</param>
-    /// <param name="offset">line offset to have a pretty tree in logs</param>
-    void drawFS(Dir currentDir, int offset=0)
-    {
-        // Just defining a culture info to have pleasant way to display big numbers, with point every 3 digits like 1.234.567.890
-        var nfi = new CultureInfo(CultureInfo.CurrentCulture.Name).NumberFormat;
-        nfi.NumberDecimalDigits = 0;             //Keep the ".00" from appearing
-        nfi.NumberGroupSeparator = ".";          //Set the group separator to a space
-        nfi.NumberGroupSizes = new int[] { 3 };  //Groups of 3 digits
-
-
-        Debug.Log(Tools2022.WriteOffset(offset) + " DIR " + currentDir.name + " (size=" + currentDir.getSize().ToString("N", nfi) + ")"
-            + System.Environment.NewLine + "Files : " + currentDir.files.Select(x=>x.size).Sum().ToString("N", nfi) + " | Dirs : " + currentDir.dirs.Select(x => x.getSize()).Sum().ToString("N", nfi)
-            );
-
-        foreach (File f in currentDir.files) { Debug.Log(Tools2022.WriteOffset(offset + 1) + f.ToString(nfi)); }
-        foreach (Dir d in currentDir.dirs)   { drawFS(d, offset + 1); }
     }
 
     /// <summary>
@@ -153,46 +175,28 @@ public class Day_2022_07 : DayScript2022
         return res;
     }
 
-    class File
+
+
+    /// <summary>
+    /// Debug function to draw filesystem tree
+    /// </summary>
+    /// <param name="currentDir">current dir to display</param>
+    /// <param name="offset">line offset to have a pretty tree in logs</param>
+    void drawFS(Dir currentDir, int offset = 0)
     {
-        public string name;
-        public double size;
-        public File(string n, double s)  { name = n; size = s; }
-        public string ToString(NumberFormatInfo nfi) { return "File '" + name + "' of size " + size.ToString("N", nfi); }
-    }
+        // Just defining a culture info to have pleasant way to display big numbers, with point every 3 digits like 1.234.567.890
+        var nfi = new CultureInfo(CultureInfo.CurrentCulture.Name).NumberFormat;
+        nfi.NumberDecimalDigits = 0;             //Keep the ".00" from appearing
+        nfi.NumberGroupSeparator = ".";          //Set the group separator to a space
+        nfi.NumberGroupSizes = new int[] { 3 };  //Groups of 3 digits
 
-    class Dir
-    {
-        public Dir parent;
-        public string name;
-        public List<File> files;
-        public List<Dir> dirs;
-        double size = -1;
 
-        public Dir(Dir dad, string n)
-        {
-            parent = dad;
-            name = n;
-            files = new List<File>();
-            dirs = new List<Dir>();
-            size = -1;
-        }
+        Debug.Log(Tools2022.WriteOffset(offset) + " DIR " + currentDir.name + " (size=" + currentDir.getSize().ToString("N", nfi) + ")"
+            + System.Environment.NewLine + "Files : " + currentDir.files.Select(x => x.size).Sum().ToString("N", nfi) + " | Dirs : " + currentDir.dirs.Select(x => x.getSize()).Sum().ToString("N", nfi)
+            );
 
-        public double getSize(int offset = 0)
-        {
-            // if size >= 0, that means size has already be computed so no need to do that again as we have no filesystem change after init
-            if (size < 0)
-            {
-                double res = 0;
-                files.ForEach(x => res += x.size);
-                dirs.ForEach(x => res += x.getSize(offset+1));
-
-                size = res;
-                //Debug.Log(Tools2022.WriteOffset(offset) + "Dir " + name + " has size " + size);
-            }
-
-            return size;
-        }
+        foreach (File f in currentDir.files) { Debug.Log(Tools2022.WriteOffset(offset + 1) + f.ToString(nfi)); }
+        foreach (Dir d in currentDir.dirs) { drawFS(d, offset + 1); }
     }
 
 }
